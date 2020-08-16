d888275807334e17a5a2f60f86ffcac7810bcda0
AndreasHolmgaard <AndreasKholmgaard@gmail.com>
2020-08-16 12:28:30 +0000

    fixed the JS bugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,60 +2,59 @@
 let clickedCard = null;
 let preventClick = false;
 let combosFound = 0;
+let attempts = 0;
 
 const colors = [
-'red', 
-'blue',
-'yellow',
-'orange',
-'tan', 
-'cyan', 
-'pink', 
-'purple', 
-'magenta', 
-'red', 
-'gold', 
-'black', 
-'brown',
-'greenyellow',
-'white',
-'silver',
-'cornflowerblue',
-'crimson',
-'chartreuse',
-'rosybrown'
+    'red',
+    'blue',
+    'yellow',
+    'orange',
+    'tan',
+    'cyan',
+    'pink',
+    'purple',
+    'magenta',
+    'red',
+    'gold',
+    'peachpuff',
+    'brown',
+    'greenyellow',
+    'white',
+    'silver',
+    'cornflowerblue',
+    'cadetblue',
+    'chartreuse',
+    'rosybrown'
 ];
- 
+
 /* generate random colors */
 
-// for(var i = 0; i < 3; i++)
-//   colors.push(Math.floor(Math.random() * 255));
+const cards = Array.from(document.querySelectorAll(".card-hard"));
 
-const cards = [document.querySelectorAll(".card-hard")];
 for (let color of colors) {
- const cardAIndex = parseInt(Math.random() * cards.length)
- const cardA = cards[cardAIndex];
- cards.splice(cardAIndex, 1);
- cardA.className += ` ${color}`
- cardA.setAttribute("data-color", color);
- 
- const cardBIndex = parseInt(Math.random() * cards.length)
- const cardB = cards[cardBIndex];
- cards.splice(cardBIndex, 1);
- cardB.className += ` ${color}`
- cardB.setAttribute("data-color", color); 
- }
+    const cardAIndex = parseInt(Math.random() * cards.length)
+    const cardA = cards[cardAIndex];
+    cards.splice(cardAIndex, 1);
+    cardA.className += ` ${color}`
+    cardA.setAttribute("data-color", color)
+
+    const cardBIndex = parseInt(Math.random() * cards.length)
+    const cardB = cards[cardBIndex];
+    cards.splice(cardBIndex, 1);
+    cardB.className += ` ${color}`
+    cardB.setAttribute("data-color", color)
+}
 
 
 function onCardClicked(e) {
     const target = e.currentTarget;
-    
-if (
-    preventClick || 
-    target === clickedCard || target.className.includes(" match") 
-){
-    return;
-}
+
+    if (
+        preventClick ||
+        target === clickedCard || target.className.includes("match")
+    ) {
+        return;
+    }
 
     target.className = target.className
         .replace("hidden", "")
@@ -65,7 +64,9 @@ if (
     if (!clickedCard) {
         // if we haven't clicked a card yet, display it's color and keep track of the card.
         clickedCard = target;
-        } else if (clickedCard) {
+    } else if (clickedCard) {
+        attempts++;
+        document.write(attempts)
         // if we have clicked a card , check if it matches the first card.
         if (
             clickedCard.getAttribute("data-color") !==
@@ -73,17 +74,19 @@ if (
         ) {
             preventClick = true;
             setTimeout(() => {
-                clickedCard.className.replace(" match", "").trim() + "hidden";
-                target.className.replace(" match", "").trim() + "hidden";
+
+                clickedCard.className = clickedCard.className.replace("match", "").trim() + " hidden";
+                target.className = target.className.replace("match", "").trim() + " hidden";
                 clickedCard = null;
                 preventClick = false;
             }, 500);
         }
-        else { 
-        clickedCard = null;
-        
+        else {
+            combosFound++;
+            clickedCard = null;
+            if (combosFound === colors.length) {
+                alert('YOU WIN!');
+            }
+        }
     }
 }
-}
-
-
